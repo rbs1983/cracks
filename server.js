349803589d28e43df77fb5f3b5f7c6e4d4548999
rbs1983 <rbs1983@gmail.com>
2026-06-25 13:48:44 +0100

    Implement '/ranking' route for player rankings
    Added a new '/ranking' route to return sorted player data.

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Servidor a funcionar 🚀");
+// dados em memória
+let players = [
+  { nome: "João", pontos: 3 },
+  { nome: "Pedro", pontos: 1 },
+  { nome: "Miguel", pontos: 5 }
+];
+
+// rota ranking
+app.get("/ranking", (req, res) => {
+  res.json(players.sort((a, b) => b.pontos - a.pontos));
 });
 
 app.listen(3000, () => {
