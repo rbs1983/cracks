b39241b77531dc47cf8d6f44c961ce776e9b87bd
rbs1983 <rbs1983@gmail.com>
2026-06-26 10:36:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 const db = new sqlite3.Database("database.db");
 
